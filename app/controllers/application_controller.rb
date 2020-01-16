7b7608a4e591aaf9ce3c387096edbab9df57f636
Randy Steele <steelerx155@gmail.com>
2020-01-16 21:53:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,10 @@ class ApplicationController < Sinatra::Base
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
+    
 
     redirect '/users/home'
+    
   end
 
   get '/sessions/login' do
