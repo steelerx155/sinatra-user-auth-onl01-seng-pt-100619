adcc140c695dcdc27ed458b6f69358a36f084dd3
Randy Steele <steelerx155@gmail.com>
2020-01-17 15:15:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,8 +47,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
-    @user = User.find(session[:user_id])
-    erb :'/users/home'
+     @user = User.find(session[:user_id])
+     erb :'/users/home'
   end
 end
